de258e853757ded39ee4c86f7f9974bf1207a886
Студент группы IS <is@sielom.local>
2026-09-21 11:45:27 +0500

    21_09

--- someapp/urls.py
+++ someapp/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import TaskListView, TaskDetailView, TagListView, TagDetailView, TaskByTagView
+from .views import (
+    TaskListView, TaskDetailView, 
+    TagListView, TagDetailView, 
+    TaskTagView, TaskByTagView
+)
 
 urlpatterns = [
     path('tasks/', TaskListView.as_view()),
     path('tasks/<int:pk>/', TaskDetailView.as_view()),
     path('tags/', TagListView.as_view()),
     path('tags/<int:pk>/', TagDetailView.as_view()),
+    path('tasks/<int:task_id>/tags/', TaskTagView.as_view()),
     path('tags/<int:tag_id>/tasks/', TaskByTagView.as_view()),
 ]
